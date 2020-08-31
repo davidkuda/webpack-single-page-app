d0be0d3e8c04651071455cb5d0f9b9958dc060bd
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-08-31 13:06:09 +0200

    initial commit

---
+++ src/client/index.js
@@ -0,0 +1,13 @@
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
