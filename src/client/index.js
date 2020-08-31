1d104acb2a83b568c4c80aab83b30d0064114de2
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-08-31 14:26:26 +0200

    fix: deletes unnecessary lines

--- src/client/index.js
+++ src/client/index.js
@@ -8,6 +8,4 @@ import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 
 console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+console.log(handleSubmit);
