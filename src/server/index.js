414ed88a70588b267db3759eb204bfebce99d6c1
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-09-04 23:25:54 +0200

    app uses now meaning cloud api instead of aylien

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
-// console.log(`Your API key is ${process.env.API_KEY}`);
+console.log(`Your API key is ${process.env.API_KEY}`);
 const path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
-const AylienNewsApi = require("aylien-news-api");
-
 
 const app = express()
 
@@ -25,18 +23,4 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
-// Aylien API
-
-const defaultClient = AylienNewsApi.ApiClient.instance;
-
-const app_id = defaultClient.authentications["app_id"];
-app_id.apiKey = process.env["NEWSAPI_APP_ID"];
-
 console.log('hello Dave 👊')
-
-const app_key = defaultClient.authentications["app_key"];
-app_key.apiKey = process.env["NEWSAPI_APP_KEY"];
-
-const api = new AylienNewsApi.DefaultApi();
-
-console.log(api)
