f71b306b5ee24705cbb2526b33436ce023b1b7e5
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-09-07 18:00:11 +0200

    feat: adds Terser Plugin to Webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,31 +2,37 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: "./src/client/index.js",
-    mode: "production",
-    output: {
-        libraryTarget: "var",
-        library: "Client",
-    },
-    module: {
-        rules: [{
-                test: "/.js$/",
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin(),
+  entry: "./src/client/index.js",
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
