43eaa3488f911b6f0dd2a8066f45fb954a80930b
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-09-07 18:04:19 +0200

    feat: adds 'Optimize CSS Assets Plugin' to Webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,13 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
   optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
   output: {
     libraryTarget: "var",
