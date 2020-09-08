1b5e02d09e2ce7f7dd68d2ca15846585cca8f2a6
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-09-08 22:03:35 +0200

    refactor: uses async/await instead of .then()

--- src/client/js/api-call.js
+++ src/client/js/api-call.js
@@ -1,11 +1,18 @@
-function apiCall(apiUrl) {
-    fetch(apiUrl)
-        .then((res) => res.json())
-        .then(function (res) {
-            console.log('hello from .then')
-            console.log(apiUrl)
-            return res
-        });
+async function apiCall(apiUrl) {
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    console.log(data)
+    return data;
 }
+
+// function apiCall(apiUrl) {
+//     fetch(apiUrl)
+//         .then((res) => res.json())
+//         .then(function (res) {
+//             console.log('hello from .then')
+//             console.log('res1:', res)
+//             return res
+//         });
+// }
 
 export { apiCall }
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,24 +4,12 @@ async function handleSubmit(event) {
     // get user input and save it into a variable
     let formInput = document.getElementById("name").value;
 
-    // // get target html element
-    // const resultsHtmlElement = document.getElementById("results");
-
     // API URL
     let apiUrl = await Client.getSentimentAnalysisUrl(formInput);
 
     // API call
     let apiResponse = await Client.apiCall(apiUrl);
     console.log('apiResponse:', apiResponse)
-
-    // function apiCall() {
-    //     fetch(apiUrl)
-    //         .then((res) => res.json())
-    //         .then(function (res) {
-    //             console.log(res);
-    //             Client.analyzeContent(resultsHtmlElement, formInput, res);
-    //         });
-    // }
 
     if (Client.checkInput(formInput)) {
         console.log('input == URL');
