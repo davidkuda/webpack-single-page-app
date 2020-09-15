c73df9394973d88fbd363b79c214e0a3e42b05ac
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-09-15 21:56:53 +0200

    fix: improves test for 'check-input'

--- src/client/js/__test__/check-input.spec.js
+++ src/client/js/__test__/check-input.spec.js
@@ -6,8 +6,6 @@ describe(
         test(
             'checkInput should return true if input contains either www or https and otherwise return false',
             () => {
-                const input = 'www';
-                const output = [];
                 expect(checkInput('www')).toBeTruthy();
                 expect(checkInput('http')).toBeTruthy();
                 expect(checkInput('anything else')).toBeFalsy();
--- src/client/js/update-ui.js
+++
@@ -1,6 +0,0 @@
-function updateUi(markup) {
-    const targetElement = document.getElementById("results");
-    targetElement.innerHTML = markup;
-};
-
-export { updateUi }
